73aadd3b6ca3e0d712db38230c24a7ef8045e83f
Sean Perkins <sean@meetmaestro.com>
2016-08-31 14:07:33 -0400

    Working Integration

--- app/main-view-model.js
+++ app/main-view-model.js
@@ -18,6 +18,10 @@ function createViewModel() {
         this.set("message", getMessage(this.counter));
     }
 
+    viewModel.api = '45644202';
+    viewModel.sessionId = '1_MX40NTY0NDIwMn5-MTQ3MjIyNzU3NTAwM35FczFWMHdVekNxeXNabWRSTUdIUkpjRmR-fg';
+    viewModel.token = 'T1==cGFydG5lcl9pZD00NTY0NDIwMiZzaWc9NTkwNmVhZWZjNDMzNWRlNDY5ZTZmZTkwMjg0Yjk0ODJlZmE4NjFjODpzZXNzaW9uX2lkPTFfTVg0ME5UWTBOREl3TW41LU1UUTNNakl5TnpVM05UQXdNMzVGY3pGV01IZFZla054ZVhOYWJXUlNUVWRJVWtwalJtUi1mZyZjcmVhdGVfdGltZT0xNDcyMjI3NTg4Jm5vbmNlPTAuNzY3MTczMTA0Njg2NjYyNiZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNDc0ODE5NTg3';
+
     return viewModel;
 }
 
